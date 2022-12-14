1886629de8417085d1e26215366229b48357efad
David Pavlík <david@dpavlik.cz>
2022-12-14 10:33:24 +0100

    Initial commit

---
+++ lib/gradient_test_project.ex
@@ -0,0 +1,18 @@
+defmodule GradientTestProject do
+  @moduledoc """
+  Documentation for `GradientTestProject`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GradientTestProject.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gradient_test_project_test.exs
@@ -0,0 +1,8 @@
+defmodule GradientTestProjectTest do
+  use ExUnit.Case
+  doctest GradientTestProject
+
+  test "greets the world" do
+    assert GradientTestProject.hello() == :world
+  end
+end
