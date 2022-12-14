3f7ecf135292efe906df21a153de1c2a73e28db7
David Pavlík <david@dpavlik.cz>
2022-12-14 22:02:13 +0100

    Add my_info function

--- lib/gradient_test_project.ex
+++ lib/gradient_test_project.ex
@@ -1,18 +1,16 @@
 defmodule GradientTestProject do
-  @moduledoc """
-  Documentation for `GradientTestProject`.
-  """
+  use Gradient.TypeAnnotation
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> GradientTestProject.hello()
-      :world
-
-  """
   def hello do
     :world
+  end
+
+  @spec my_info() :: Keyword.t()
+  def my_info do
+    # the returned value cannot be nil,
+    # as "process can make calls" implies "process is alive",
+    # so we assert that it is indeed a keyword list
+    Process.info(self())
+    |> assert_type(Keyword.t())
   end
 end
